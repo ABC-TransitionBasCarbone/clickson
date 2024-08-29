a8772a5c0f0538123c134be81ddd7a4ebc6e91ec
RomainCrevecoeur <romain.crevecoeur@abc-transitionbascarbone.fr>
2024-08-29 16:58:25 +0200

    clean middleware

--- middleware.ts
+++ middleware.ts
@@ -4,8 +4,8 @@ import { updateSession } from "./lib";
 export async function middleware(request: NextRequest) {
   const session = request.cookies.has('session')
   if (session && (request.nextUrl.pathname === "/")) {
-   return NextResponse.redirect(new URL('/dashboard', request.url))
-  } else if (!session && (request.nextUrl.pathname !== "/") && (request.nextUrl.pathname !== "/signup")&& (request.nextUrl.pathname !== "/csv")) {
+    return NextResponse.redirect(new URL('/dashboard', request.url))
+  } else if (!session && (request.nextUrl.pathname !== "/") && (request.nextUrl.pathname !== "/signup")) {
     return NextResponse.redirect(new URL('/', request.url))
   }
   return await updateSession(request);
