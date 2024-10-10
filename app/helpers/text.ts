e1ef81a04315b255ef4023b28504723769d2a5ff
RomainCrevecoeur <romain.crevecoeur@abc-transitionbascarbone.fr>
2024-10-10 17:02:39 +0200

    energy page functionnal

---
+++ app/helpers/text.ts
@@ -0,0 +1,10 @@
+export const prepareValueForTranslation = (value: string) => {
+    let newValue;
+
+    newValue = value.toLowerCase()
+            .replace(/\s+/g, "-")
+            .replace(/\s/g, "")
+            .replace(/-\([^)]*\)$/, "");
+
+    return `abc-${newValue}`;
+}
